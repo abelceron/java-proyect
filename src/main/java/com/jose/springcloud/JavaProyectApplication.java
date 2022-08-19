d26ef711b8417a35572c409c1cb037334f2e8cff
Abel <joseabel298@gmail.com>
2022-08-19 09:35:00 -0600

    prueba

---
+++ src/main/java/com/jose/springcloud/JavaProyectApplication.java
@@ -0,0 +1,13 @@
+package com.jose.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jose/springcloud/JavaProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
